9c24e0b9a8ac9917803af67fd4e86d6cbd9d3058
卄ㄚ卩乇尺几ㄖᐯ卂✨ <mhmmdeymngrbz@gmail.com>
2026-05-17 20:47:43 +0300

    Update samples.cpp

--- samples.cpp
+++ samples.cpp
@@ -1,1 +1,13 @@
+#include "include/logger.hpp"
 
+int main()
+{
+    Logger logger("app.log");
+
+    logger.log(LogLevel::INFO, "liblogger-dev successfully initialized without namespace.");
+    logger.log(LogLevel::WARNING, "Testing asynchronous queue processing speed.");
+    logger.log(LogLevel::ERROR, "Simulated error log entry for TIG testing.");
+    logger.log(LogLevel::CRITICAL, "Critical system check complete.");
+
+    return 0;
+}
